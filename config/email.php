d784a1a1600311b7d11683772c95a9b1dfef9ce0
gamboa-romel-panlilio <melgrowtopia029@gmail.com>
2025-02-02 21:48:26 +0800

    added by "Romel Gamboa"

---
+++ config/email.php
@@ -0,0 +1,34 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/../includes/phpmailer/src/Exception.php';
+require_once __DIR__ . '/../includes/phpmailer/src/PHPMailer.php';
+require_once __DIR__ . '/../includes/phpmailer/src/SMTP.php';
+
+function sendEmail($to, $subject, $message) {
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'amabeyonce0@gmail.com'; 
+        $mail->Password   = 'uvgdeyhogpjtdjlg'; 
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+
+        $mail->setFrom('amabeyonce0@gmail.com', 'Task Manager');
+        $mail->addAddress($to);
+
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $message;
+
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        return false;
+    }
+}
+?>
