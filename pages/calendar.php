aedd0eb141ea6631137faccab172f482646ce365
arpon-jolas-santos <jolasarpon1@gmail.com>
2025-02-05 21:30:47 +0800

    editted by "Jolas Arpon"

--- pages/calendar.php
+++ pages/calendar.php
@@ -63,26 +63,14 @@ checkSession();
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                        <a class="nav-link" href="dashboard.php">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="calendar.php">Calendar</a>
-                    </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="tasks_by_priority.php">Tasks by Priority</a>
-                    </li>
-                     <li class="nav-item">
-                        <a class="nav-link " href="all_tasks.php">In Progress Tasks</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="completed_tasks.php">Completed Tasks</a>
-                    </li>
-                   
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" onclick="return confirmLogout();">Logout</a>
-                    </li>
+            <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">  <a class="nav-link" href="dashboard.php">Dashboard</a></li> 
+                    <li class="nav-item"> <a class="nav-link active" href="calendar.php">Calendar</a></li>
+                    <li class="nav-item"><a class="nav-link" href="tasks_by_priority.php">Tasks by Priority</a></li>
+                    <li class="nav-item"><a class="nav-link" href="all_tasks.php">In Progress Tasks</a></li>
+                    <li class="nav-item"><a class="nav-link" href="completed_tasks.php">Completed Tasks</a></li>
+                    <li class="nav-item"><a class="nav-link" href="settings.php">Settings</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" onclick="return confirmLogout();">Logout</a></li>
                 </ul>
             </div>
         </div>
